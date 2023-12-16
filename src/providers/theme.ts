5aa34d01b76afb89456e1726324ad41ebfd18889
Kalmovic <joseph.ext@rallyrd.com>
2023-12-16 13:25:26 -0300

    wip: theme

---
+++ src/providers/theme.ts
@@ -0,0 +1,33 @@
+export const theme = {
+  buttons: {
+    primary: {
+      background: "#4C4DDC",
+      color: "#fff",
+    },
+    secondary: {
+      background: "#fff",
+      color: "#354e9a",
+      borderColor: "#D6D6D6",
+    },
+    danger: {
+      background: "#9a3535",
+      color: "#fff",
+    },
+    disabled: {
+      background: "#EDEDED",
+      color: "#878787",
+    },
+  },
+  colors: {
+    primary: "#4C4DDC",
+    secondary: "#354e9a",
+    surface: "rgba(245, 245, 255, 0.50)",
+    primaryLight: "#E6E6FF",
+    danger: "#9a3535",
+    disabled: "#EDEDED",
+    white: "#fff",
+    black: "#000",
+    gray: "#878787",
+    lightGray: "#D6D6D6",
+  },
+};
