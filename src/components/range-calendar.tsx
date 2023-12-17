15070ddfb9224b9280e81cee9003fd91c2e8f938
Kalmovic <joseph.ext@rallyrd.com>
2023-12-16 22:07:54 -0300

    feat: add pricing for each day on calendar

--- src/components/range-calendar.tsx
+++ src/components/range-calendar.tsx
@@ -7,12 +7,27 @@ import {
   DateValue,
 } from "react-aria-components";
 import "./range-calendar.css";
-import { today, getLocalTimeZone } from "@internationalized/date";
-import { IconButton } from "@radix-ui/themes";
+import {
+  today,
+  getLocalTimeZone,
+  CalendarDate,
+  toCalendarDate,
+} from "@internationalized/date";
+import { IconButton, Text } from "@radix-ui/themes";
 import { ArrowLeftIcon, ArrowRightIcon } from "@radix-ui/react-icons";
+import { useDateFormatter } from "react-aria";
 
-export function BookCalendar(props) {
+type BookCalenderProps = {
+  onChange: (date: { start: string; end: string; totalPrice: number }) => void;
+};
+
+export function BookCalendar(props: BookCalenderProps) {
   const now = today(getLocalTimeZone());
+  const formatter = useDateFormatter({
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+  });
   const disabledRanges = [
     [now, now.add({ days: 5 })],
     [now.add({ days: 14 }), now.add({ days: 16 })],
@@ -24,6 +39,38 @@ export function BookCalendar(props) {
       (interval) =>
         date.compare(interval[0]) >= 0 && date.compare(interval[1]) <= 0
     );
+
+  const isWeekend = (date: CalendarDate) => {
+    return date.toDate(getLocalTimeZone()).getDay() === 0 ||
+      date.toDate(getLocalTimeZone()).getDay() === 6
+      ? true
+      : false;
+  };
+
+  const calculateTotalPrice = (start: DateValue, end: DateValue) => {
+    let totalPrice = 0;
+
+    for (
+      let currentDate = start;
+      currentDate.compare(end) <= 0;
+      currentDate = currentDate.add({ days: 1 })
+    ) {
+      const date = toCalendarDate(currentDate);
+      const price = isWeekend(date) ? 200 : 100;
+      totalPrice += price;
+    }
+
+    return totalPrice;
+  };
+
+  const onChangeDates = (date: RangeValue<DateValue>) => {
+    // array of dates between start and end
+    return props.onChange({
+      start: formatter.format(date.start.toDate(getLocalTimeZone())),
+      end: formatter.format(date.end.toDate(getLocalTimeZone())),
+      totalPrice: calculateTotalPrice(date.start, date.end),
+    });
+  };
   return (
     <RangeCalendar
       aria-label="Trip dates"
@@ -31,12 +78,19 @@ export function BookCalendar(props) {
       pageBehavior="visible"
       minValue={now}
       isDateUnavailable={isDateUnavailable}
+      onChange={onChangeDates}
     >
       <header>
         <IconButton slot="prev">
           <ArrowLeftIcon />
         </IconButton>
-        <Heading />
+        <Heading
+          style={{
+            fontSize: 16,
+            fontWeight: "bold",
+            margin: 0,
+          }}
+        />
         <IconButton slot="next">
           <ArrowRightIcon />
         </IconButton>
@@ -47,7 +101,26 @@ export function BookCalendar(props) {
             <CalendarCell
               date={date}
               onHoverStart={(date) => console.log(date.target.ariaLabel)}
-            />
+              style={{
+                gap: 0.2,
+              }}
+            >
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  gap: 0.2,
+                  width: "40px",
+                  height: "40px",
+                }}
+              >
+                <Text size="2">{date.day}</Text>
+                {/* price based on weekday (if weekend, double)*/}
+                <Text size="1">{isWeekend(date) ? 200 : 100}</Text>
+              </div>
+            </CalendarCell>
           )}
         </CalendarGrid>
         <CalendarGrid offset={{ months: 1 }}>
