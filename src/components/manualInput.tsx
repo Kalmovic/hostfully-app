270d8d9bdc26642bd4d8bf6b4e9d0f24177c7adb
Kalmovic <joseph.ext@rallyrd.com>
2023-12-17 22:32:38 -0300

    feat: manual input component

---
+++ src/components/manualInput.tsx
@@ -0,0 +1,51 @@
+import { PlusIcon, MinusIcon } from "@radix-ui/react-icons";
+import { Flex, IconButton, Text } from "@radix-ui/themes";
+import React from "react";
+
+type ManualInputProps = {
+  label: string;
+  initialValue: string;
+  minimumValue?: string;
+  onChange: (value: number) => void;
+};
+
+export function ManualInput({
+  label,
+  initialValue,
+  minimumValue = "0",
+  onChange,
+}: ManualInputProps) {
+  const [value, setValue] = React.useState(initialValue);
+  return (
+    <Flex gap="2" align="center">
+      <Text>{label}</Text>
+      <Flex direction="row" align="center" gap="2">
+        <IconButton
+          disabled={Number(value) === Number(minimumValue)}
+          variant="soft"
+          color="indigo"
+          type="button"
+          onClick={() => {
+            setValue(String(Number(value) - 1));
+            onChange(Number(value) - 1);
+          }}
+        >
+          <MinusIcon />
+        </IconButton>
+        <Text>{value}</Text>
+        <IconButton
+          disabled={Number(value) === 10}
+          variant="soft"
+          color="indigo"
+          type="button"
+          onClick={() => {
+            setValue(String(Number(value) + 1));
+            onChange(Number(value) + 1);
+          }}
+        >
+          <PlusIcon />
+        </IconButton>
+      </Flex>
+    </Flex>
+  );
+}
