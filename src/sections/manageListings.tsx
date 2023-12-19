4f6e4b0caf86930f38f5a39f683187af0c855d31
Kalmovic <joseph.ext@rallyrd.com>
2023-12-19 09:57:22 -0300

    add empty screen to manage listings

--- src/sections/manageListings.tsx
+++ src/sections/manageListings.tsx
@@ -2,12 +2,50 @@ import { Table } from "../components/table";
 import styled from "styled-components";
 import { Flex, Text } from "@radix-ui/themes";
 import { useBookingStore } from "../providers/bookingsProvider";
+import { Info } from "lucide-react";
 
 const Wrapper = styled.section({
   display: "flex",
   flexDirection: "column",
   width: "100%",
 });
+
+const EmptyBookings = () => (
+  <Flex
+    direction="column"
+    align="center"
+    justify="center"
+    gap="5"
+    style={{
+      height: "100%",
+      padding: 16,
+      borderRadius: 8,
+      width: "fit-content",
+      alignSelf: "center",
+      marginTop: 64,
+    }}
+  >
+    <Info size={64} />
+    <Text
+      as="span"
+      size="4"
+      style={{
+        marginBottom: 16,
+      }}
+    >
+      You have no bookings yet.
+    </Text>
+    <Text
+      as="span"
+      size="4"
+      style={{
+        marginBottom: 16,
+      }}
+    >
+      You can book a hotel from the Explore tab.
+    </Text>
+  </Flex>
+);
 
 export function ManageListings() {
   const bookings = useBookingStore((state) => state.bookings);
@@ -63,30 +101,36 @@ export function ManageListings() {
 
   return (
     <Wrapper>
-      <Text
-        as="span"
-        size="8"
-        style={{
-          marginBottom: 16,
-        }}
-      >
-        Bookings
-      </Text>
-      <Flex gap="4">
-        <Table
-          actions={["edit", "cancel"]}
-          headers={[
-            "id",
-            "Name",
-            "Period",
-            "Booking Details",
-            "Price",
-            "Status",
-            "Actions",
-          ]}
-          rows={rows}
-        />
-      </Flex>
+      {bookings.length === 0 ? (
+        <EmptyBookings />
+      ) : (
+        <>
+          <Text
+            as="span"
+            size="8"
+            style={{
+              marginBottom: 16,
+            }}
+          >
+            Manage your bookings
+          </Text>
+          <Flex gap="4">
+            <Table
+              actions={["edit", "cancel"]}
+              headers={[
+                "id",
+                "Name",
+                "Period",
+                "Booking Details",
+                "Price",
+                "Status",
+                "Actions",
+              ]}
+              rows={rows}
+            />
+          </Flex>
+        </>
+      )}
     </Wrapper>
   );
 }
