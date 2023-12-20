eca73b7c9f1a9b0c8e17793faf1f74fbcafccc8d
Kalmovic <joseph.ext@rallyrd.com>
2023-12-19 23:30:39 -0300

    feat: add unformat util

--- src/utils/formatCurrency.ts
+++ src/utils/formatCurrency.ts
@@ -1,4 +1,9 @@
 export const formatToDollar = new Intl.NumberFormat("en-US", {
   style: "currency",
   currency: "USD",
+  minimumFractionDigits: 0,
 });
+
+export const unformatFromDollar = (value: string) => {
+  return Number(value.replace(/[^0-9.-]+/g, ""));
+};
