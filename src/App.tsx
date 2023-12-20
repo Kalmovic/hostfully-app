cc1e1eecd40ebbcd68b52b9548a6ab8c7f0eac7a
Kalmovic <joseph.ext@rallyrd.com>
2023-12-20 01:57:41 -0300

    feat: add logo and limit width

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Text } from "@radix-ui/themes";
 import { ManageBookings } from "./sections/manageBookings";
 import styled from "styled-components";
 import { Explore } from "./sections/explore";
+import logo from "./assets/hostfully-logo.svg";
 
 const AppWrapper = styled.div(({ theme }) => ({
   display: "flex",
@@ -10,19 +11,28 @@ const AppWrapper = styled.div(({ theme }) => ({
   width: "100%",
   height: "100vh",
   backgroundColor: theme.colors.lightBlue,
+  maxWidth: "1400px",
+  margin: "0 auto",
 }));
 function App() {
   return (
     <AppWrapper>
       <Text
         style={{
-          fontSize: 42,
+          display: "flex",
+          fontSize: 28,
           marginBottom: 16,
           marginTop: 16,
           marginLeft: 16,
+          alignItems: "baseline",
         }}
       >
-        Welcome!
+        Welcome to{" "}
+        <img
+          src={logo}
+          alt="logo"
+          style={{ width: 150, position: "relative", top: 8, left: 8 }}
+        />
       </Text>
       <Tabs
         tabs={[
